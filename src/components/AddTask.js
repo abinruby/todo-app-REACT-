f2ccb88e7ad0065dce87ff1d1e7dafd722815123
Abin K Ruby <abinruby51@gmail.com>
2022-12-08 01:34:08 +0530

    Finished React todo app

---
+++ src/components/AddTask.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+const AddTask = ({addTask}) => {
+    const [value,setValue]=useState('')
+    const addItem=()=>{
+        addTask(value)
+        setValue('')
+    }
+  return (
+    <>
+        <div className='input-container'>
+            <input 
+            type='text'
+            className='input'
+            placeholder='Add a new Task'
+            value={value}
+            onChange={(e)=>{
+                setValue(e.target.value)
+            }}
+             />
+            <button onClick={addItem} className='add-btn'>ADD</button>
+        </div>
+    </>
+  )
+}
+
+export default AddTask
